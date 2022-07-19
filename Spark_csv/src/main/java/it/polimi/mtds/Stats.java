fdb88270a1dc2811800f6426eca416142b175ae3
Matteo Visotto <matteo.visotto@mail.polimi.it>
2022-07-19 17:07:41 +0200

    Queries

--- Spark_csv/src/main/java/it/polimi/mtds/Stats.java
+++ Spark_csv/src/main/java/it/polimi/mtds/Stats.java
@@ -50,11 +50,13 @@ public class Stats {
 
         WindowSpec w = (Window.partitionBy(col("room")).orderBy(col("dateTime").cast(DataTypes.LongType)).rangeBetween(-7, 0));
 
-        dataset = dataset.withColumn("neighborhoods",split(col("location"),"[.]").getItem(0))
+        dataset = dataset.withColumn("neighborhood",split(col("location"),"[.]").getItem(0))
                 .withColumn("building",split(col("location"),"[.]").getItem(1))
                 .withColumn("floor",split(col("location"),"[.]").getItem(2))
                 .withColumn("room",split(col("location"),"[.]").getItem(3))
-                .withColumnRenamed("location", "fullLocation")
+                .withColumn("fullFloorLocation", concat_ws(".", col("neighborhood"), col("building"), col("floor")))
+                .withColumn("fullBuildingLocation", concat_ws(".", col("neighborhood"), col("building")))
+                .withColumnRenamed("location", "fullRoomLocation")
                 .withColumn("hour", hour(col("dateTime")))
                 .withColumn("day", to_date(col("dateTime")))
                 .withColumn("week", weekofyear(col("dateTime")))
@@ -62,7 +64,7 @@ public class Stats {
                 .withColumn("year", year(col("dateTime")))
                 .withColumn("night", hour(col("dateTime")).cast(DataTypes.IntegerType).lt(8).or(hour(col("dateTime")).cast(DataTypes.IntegerType).geq(20)))
                 .withColumn("daily", hour(col("dateTime")).cast(DataTypes.IntegerType).lt(20).and(hour(col("dateTime")).cast(DataTypes.IntegerType).geq(8)))
-                .withColumn("rolling_average", avg("temperature").over(w));
+        ;
         dataset.show();
         dataset.cache();
 
@@ -77,8 +79,6 @@ public class Stats {
 
         //movingAverageTemperatureAndHumidityHour.withColumn("rolling_average", avg("dollars").over(w));
 
-        movingAverageTemperatureAndHumidityHour.show();
-        //SU QUESTA MANCA ROOM MENTRE SU QUELLA SOTTO OK BUILDING MA NON RAGGRUPPATO PER GIORNO.
 
 
         //Hourly moving average - Building
@@ -108,7 +108,7 @@ public class Stats {
                 )
                 .orderBy("week");
 
-        movingAverageTemperatureAndHumidityDaily.show();
+
 
 
         //Daily moving average - Building
@@ -132,7 +132,6 @@ public class Stats {
                 )
                 .orderBy("week");
 
-        movingAverageTemperatureAndHumidityWeekly.show();
 
 
         //Weekly moving average - Building Level
@@ -151,87 +150,135 @@ public class Stats {
 
 
 
-        //Day temperature -> average temperature between 8am and 8pm
-        //Night temperature -> average temperature between 8pm and 8am
         //Daily night-day temperature difference - Room
-        final Dataset<Row> meanDaily = dataset
-                .groupBy("daily", "day", "room")
+        final Dataset<Row> meanDailyRoom = dataset
+                .groupBy("daily", "day", "fullRoomLocation")
                 .agg(
                         avg("temperature").as("avg_temp"),
                         avg("humidity").as("avg_hum")
                 )
                 .orderBy("day");
-                /*.rdd()
-                .zipWithIndex()
-                .foreach((a,b) -> {
 
-                });*/
+        final Dataset<Row> dailyRoom = meanDailyRoom.filter(col("daily").equalTo(true)).withColumnRenamed("avg_temp", "day_temp").withColumnRenamed("avg_hum", "day_hum");
+        final Dataset<Row> nightRoom = meanDailyRoom.filter(col("daily").equalTo(false)).withColumnRenamed("fullRoomLocation", "fl").withColumnRenamed("day", "d").withColumnRenamed("avg_temp", "night_temp").withColumnRenamed("avg_hum", "night_hum");
 
-        meanDaily.cache();
-        meanDaily.show();
+        final Dataset<Row> diffRoom = dailyRoom.join(nightRoom, nightRoom.col("fl").equalTo(dailyRoom.col("fullRoomLocation")).and(nightRoom.col("d").equalTo(dailyRoom.col("day"))), "left_outer")
+                .filter(col("d").isNotNull())
+                .drop("daily", "d", "fl")
+                .withColumn("temp_diff", expr("day_temp-night_temp"))
+                .withColumn("hum_diff", expr("day_hum-night_hum"));
+        diffRoom.cache();
 
+
+        //Daily night-day temperature difference - Floor Level
+        final Dataset<Row> meanDailyFloor = dataset
+                .groupBy("daily", "day", "fullFloorLocation")
+                .agg(
+                        avg("temperature").as("avg_temp"),
+                        avg("humidity").as("avg_hum")
+                )
+                .orderBy("day");
+
+        final Dataset<Row> dailyFloor = meanDailyFloor.filter(col("daily").equalTo(true)).withColumnRenamed("avg_temp", "day_temp").withColumnRenamed("avg_hum", "day_hum");
+        final Dataset<Row> nightFloor = meanDailyFloor.filter(col("daily").equalTo(false)).withColumnRenamed("fullFloorLocation", "fl").withColumnRenamed("day", "d").withColumnRenamed("avg_temp", "night_temp").withColumnRenamed("avg_hum", "night_hum");
+
+        final Dataset<Row> diffFloor = dailyFloor.join(nightFloor, nightFloor.col("fl").equalTo(dailyFloor.col("fullFloorLocation")).and(nightFloor.col("d").equalTo(dailyFloor.col("day"))), "left_outer")
+                .filter(col("d").isNotNull())
+                .drop("daily", "d", "fl")
+                .withColumn("temp_diff", expr("day_temp-night_temp"))
+                .withColumn("hum_diff", expr("day_hum-night_hum"));
+        diffFloor.cache();
+        
         //Daily night-day temperature difference - Building Level
+        final Dataset<Row> meanDailyBuilding = dataset
+                .groupBy("daily", "day", "fullBuildingLocation")
+                .agg(
+                        avg("temperature").as("avg_temp"),
+                        avg("humidity").as("avg_hum")
+                )
+                .orderBy("day");
 
+        final Dataset<Row> dailyBuilding = meanDailyBuilding.filter(col("daily").equalTo(true)).withColumnRenamed("avg_temp", "day_temp").withColumnRenamed("avg_hum", "day_hum");
+        final Dataset<Row> nightBuilding = meanDailyBuilding.filter(col("daily").equalTo(false)).withColumnRenamed("fullBuildingLocation", "fl").withColumnRenamed("day", "d").withColumnRenamed("avg_temp", "night_temp").withColumnRenamed("avg_hum", "night_hum");
 
-
-        //Daily night-day temperature difference - Building
-
+        final Dataset<Row> diffBuilding = dailyBuilding.join(nightBuilding, nightBuilding.col("fl").equalTo(dailyBuilding.col("fullBuildingLocation")).and(nightBuilding.col("d").equalTo(dailyBuilding.col("day"))), "left_outer")
+                .filter(col("d").isNotNull())
+                .drop("daily", "d", "fl")
+                .withColumn("temp_diff", expr("day_temp-night_temp"))
+                .withColumn("hum_diff", expr("day_hum-night_hum"));
+        diffBuilding.cache();
 
 
         //Daily night-day temperature difference - Neighborhood-scale
+        final Dataset<Row> meanDailyNeighborhood = dataset
+                .groupBy("daily", "day", "neighborhood")
+                .agg(
+                        avg("temperature").as("avg_temp"),
+                        avg("humidity").as("avg_hum")
+                )
+                .orderBy("day");
 
+        final Dataset<Row> dailyNeighborhood = meanDailyNeighborhood.filter(col("daily").equalTo(true)).withColumnRenamed("avg_temp", "day_temp").withColumnRenamed("avg_hum", "day_hum");
+        final Dataset<Row> nightNeighborhood = meanDailyNeighborhood.filter(col("daily").equalTo(false)).withColumnRenamed("neighborhood", "fl").withColumnRenamed("day", "d").withColumnRenamed("avg_temp", "night_temp").withColumnRenamed("avg_hum", "night_hum");
+
+        final Dataset<Row> diffNeighborhood = dailyNeighborhood.join(nightNeighborhood, nightNeighborhood.col("fl").equalTo(dailyNeighborhood.col("neighborhood")).and(nightNeighborhood.col("d").equalTo(dailyNeighborhood.col("day"))), "left_outer")
+                .filter(col("d").isNotNull())
+                .drop("daily", "d", "fl")
+                .withColumn("temp_diff", expr("day_temp-night_temp"))
+                .withColumn("hum_diff", expr("day_hum-night_hum"));
+        diffNeighborhood.cache();
 
 
         //Month of the year with higher average night-day temperature difference - Room
-
-
-
+        final Dataset<Row> avgMonthRoom = diffRoom.withColumn("month", month(col("day"))).groupBy("month")
+                .agg(
+                        avg("temp_diff").as("temp"),
+                        avg("hum_diff").as("hum")
+                )
+                .orderBy("month");
+        avgMonthRoom.cache();
+        avgMonthRoom.show();
+        final Dataset<Row> maxMonthRoom = avgMonthRoom.select(col("month"), col("temp")).where(col("temp").equalTo(avgMonthRoom.select(max("temp")).first().getDouble(0)));
+        maxMonthRoom.show();
+        
         //Month of the year with higher average night-day temperature difference - Building Level
-
+        final Dataset<Row> avgMonthFloor = diffFloor.withColumn("month", month(col("day"))).groupBy("month")
+                .agg(
+                        avg("temp_diff").as("temp"),
+                        avg("hum_diff").as("hum")
+                )
+                .orderBy("month");
+        avgMonthFloor.cache();
+        avgMonthFloor.show();
+        final Dataset<Row> maxMonthFloor = avgMonthFloor.select(col("month"), col("temp")).where(col("temp").equalTo(avgMonthFloor.select(max("temp")).first().getDouble(0)));
+        maxMonthFloor.show();
 
 
         //Month of the year with higher average night-day temperature difference - Building
-
+        final Dataset<Row> avgMonthBuilding = diffBuilding.withColumn("month", month(col("day"))).groupBy("month")
+                .agg(
+                        avg("temp_diff").as("temp"),
+                        avg("hum_diff").as("hum")
+                )
+                .orderBy("month");
+        avgMonthBuilding.cache();
+        avgMonthBuilding.show();
+        final Dataset<Row> maxMonthBuilding = avgMonthBuilding.select(col("month"), col("temp")).where(col("temp").equalTo(avgMonthBuilding.select(max("temp")).first().getDouble(0)));
+        maxMonthBuilding.show();
 
 
         //Month of the year with higher average night-day temperature difference - Neighborhood-scale
+        final Dataset<Row> avgMonthNeighborhood = diffNeighborhood.withColumn("month", month(col("day"))).groupBy("month")
+                .agg(
+                        avg("temp_diff").as("temp"),
+                        avg("hum_diff").as("hum")
+                )
+                .orderBy("month");
+        avgMonthNeighborhood.cache();
+        avgMonthNeighborhood.show();
+        final Dataset<Row> maxMonthNeighborhood = avgMonthNeighborhood.select(col("month"), col("temp")).where(col("temp").equalTo(avgMonthNeighborhood.select(max("temp")).first().getDouble(0)));
+        maxMonthNeighborhood.show();
 
-
-
-
-
-
-        /*final long maxTotal = sumWithdrawals
-                .agg(max("sum(amount)"))
-                .first()
-                .getLong(0);
-
-        final Dataset<Row> maxWithdrawals = sumWithdrawals
-                .filter(sumWithdrawals.col("sum(amount)").equalTo(maxTotal));
-
-        maxWithdrawals.show();
-
-        // Q3 Accounts with negative balance
-
-        final Dataset<Row> totWithdrawals = withdrawals
-                .groupBy("account")
-                .sum("amount")
-                .drop("person")
-                .as("totalWithdrawals");
-
-        final Dataset<Row> totDeposits = deposits
-                .groupBy("account")
-                .sum("amount")
-                .drop("person")
-                .as("totalDeposits");
-
-        final Dataset<Row> negativeAccounts = totWithdrawals
-                .join(totDeposits, totDeposits.col("account").equalTo(totWithdrawals.col("account")), "left_outer")
-                .filter(totDeposits.col("sum(amount)").isNull().and(totWithdrawals.col("sum(amount)").gt(0)).or
-                                (totWithdrawals.col("sum(amount)").gt(totDeposits.col("sum(amount)")))
-                ).select(totWithdrawals.col("account"));
-
-        negativeAccounts.show();*/
 
         spark.close();
 
