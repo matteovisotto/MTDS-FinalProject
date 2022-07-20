142dc114e0bd05810c6d4ff87bad31951621952c
TiaSirio <10571322@polimi.it>
2022-07-20 11:56:08 +0200

    Change size of dataset.

--- MTDS-Dataset PythonCode/main.py
+++ MTDS-Dataset PythonCode/main.py
@@ -43,7 +43,7 @@ def generate_fake_data(n):
 
 print("Starting...")
 
-temp, hum, datetime, location = generate_fake_data(40000)
+temp, hum, datetime, location = generate_fake_data(100000)
 
 df = pd.DataFrame([])
 df['Temperature'] = temp
